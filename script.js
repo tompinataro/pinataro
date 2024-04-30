51df6139b44f4d968467af537f4f19d9cf2380d1
Tom Pinataro <tompinataro@gmail.com>
2024-04-29 22:28:27 -0500

    script clean up

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function countdownTimer(targetDate) {
             document.getElementById('countdown').innerHTML = "Time's up!";
             return;
         }
-
         const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
         const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
@@ -16,7 +15,5 @@ function countdownTimer(targetDate) {
         document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m`;
     }, 1000);
 }
-
 const targetDate = new Date('October 19, 2024 00:09:00');
-
 countdownTimer(targetDate);
