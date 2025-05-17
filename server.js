bb6519ff5fb77e6d306486635859bddf5e915c46
Tom Pinataro <tompinataro@gmail.com>
2025-05-17 13:42:44 -0500

    Add Console.log to debug

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Log every request to help with debugging
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
