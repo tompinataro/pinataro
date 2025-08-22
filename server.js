6bc113e870e2f3f67de100a217faf1fd96bb61ca
Tom Pinataro <tompinataro@gmail.com>
2025-08-21 20:43:05 -0500

    heroku(node): express static server to bind

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const rootDir = path.join(__dirname, '.'); // change to 'public' if your index.html is there
+const rootDir = path.join(__dirname, '.'); // change to 'public' if your files live there
 app.use(express.static(rootDir));
 app.get('*', (_, res) => res.sendFile(path.join(rootDir, 'index.html')));
 const port = process.env.PORT || 3000;
